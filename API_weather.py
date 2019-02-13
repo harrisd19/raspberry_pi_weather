f1b1b1c244e3ce96a5f06957d997ebb36bd6fa6d
Devon <stdsh07@moravian.edu>
2019-02-13 15:38:11 -0500

    Extracted external api call to open weather maps to one time call function

--- API_weather.py
+++ API_weather.py
@@ -1,5 +1,11 @@
 import requests
 
+def get_all_weather():
+    payload = {'id':'5180225', 'appid':'b11cb1a1d02b30e459bd6c5ce5615c1e', 'units':'imperial', 'mode':'json'}
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather', params = payload)
+    weather_data = response.json()
+    return weather_data
+
 def get_temp(all_weather_data):
     current_temp = all_weather_data['main']['temp']
     return current_temp
@@ -9,13 +15,9 @@ def get_city(all_weather_data):
     return selected_city
 
 def main():
-    payload = {'id':'5180225', 'appid':'b11cb1a1d02b30e459bd6c5ce5615c1e', 'units':'imperial', 'mode':'json'}
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather', params = payload)
-    weather_data = response.json()    
-
-    print(response.text)
-    print (get_temp(weather_data))
-    print (get_city(weather_data))
+    all_weather = get_all_weather()
+    print (get_temp(all_weather))
+    print (get_city(all_weather))
 
 if __name__ == "__main__":
     main()
