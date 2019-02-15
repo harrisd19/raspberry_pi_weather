da8312557b8ad1b094a0a4b5b958e50e50482615
Devon <stdsh07@moravian.edu>
2019-02-15 09:25:09 -0500

    edited response type for current conditions and added wind conditions function.

--- API_weather.py
+++ API_weather.py
@@ -15,14 +15,20 @@ def get_city(all_weather_data):
     selected_city = all_weather_data['name']
     return selected_city
 
+def get_current_wind_condition(all_weather_data):
+    wind_direction_in_degrees = int(all_weather_data['wind']['deg'])
+    wind_speed_in_mph = int(all_weather_data['wind']['speed'])
+
+    return wind_direction_in_degrees, wind_speed_in_mph
+
 def get_current_conditions(all_weather_data):
-    current_condition_code = all_weather_data['weather'][0]['id']
-    current_condition_main = all_weather_data['weather'][0]['main']
-    current_condition_description = all_weather_data['weather'][0]['description']
-    conditions_list = [current_condition_code, current_condition_main, current_condition_description]
+    current_condition_code = int(all_weather_data['weather'][0]['id'])
+    current_condition_main = str(all_weather_data['weather'][0]['main'])
+    current_condition_description = str(all_weather_data['weather'][0]['description'])
+
     weather_icon = get_weather_icon(all_weather_data['weather'][0]['icon'])
 
-    return weather_icon, conditions_list
+    return weather_icon, current_condition_code, current_condition_main, current_condition_description
      
 def get_weather_icon(icon_code):
     icon_url = 'http://openweathermap.org/img/w/' + icon_code + '.png'
@@ -32,6 +38,7 @@ def get_weather_icon(icon_code):
 
 def main():
     all_weather = get_all_weather()
+    print (get_current_wind_condition(all_weather))
     print (get_current_conditions(all_weather))
     print (get_temp(all_weather))
     print (get_city(all_weather))
